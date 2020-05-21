22c8cae0db1579daa48d586a04fcafc03aa34103
Matthias Endler <matthias-endler@gmx.net>
2020-05-21 23:06:57 +0200

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        color1: "#61a0afff",
-        color2: "#96c9dcff",
-        color3: "#f06c9bff",
+        color1: "#a2dff2ff",
+        color2: "#a8ceffff",
+        color3: "#9ba5d9ff",
         color4: "#f9b9b7ff",
         color5: "#f5d491ff",
       },
