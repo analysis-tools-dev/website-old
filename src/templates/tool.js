c50abf8d7f161c26830044765d30c0a1086caedc
Matthias Endler <matthias-endler@gmx.net>
2020-05-22 20:25:48 +0200

    Icons

--- src/templates/tool.js
+++ src/templates/tool.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import { Helmet } from "react-helmet"
 import "twin.macro"
+import { FaTags, FaHome } from "react-icons/fa"
 
 export default function BlogPost(d) {
   const tool = d.data.toolsYaml
@@ -16,14 +17,20 @@ export default function BlogPost(d) {
         <div tw="bg-white flex flex-col justify-start p-6 w-full">
           <h1 tw="text-3xl font-semibold pb-10">{tool.name}</h1>
           <p>{tool.description}</p>
-          <a href={tool.url}>{tool.url}</a>
-          <ul tw="flex flex-wrap list-none max-w-sm">
-            {tool.tags.map(tag => (
-              <li tw="mt-1 mr-1 mb-1" key={tool.id}>
-                <span tw="bg-color1 px-1 py-1">{tag}</span>
-              </li>
-            ))}
-          </ul>
+          <div tw="flex">
+            <FaHome tw="mt-1 mr-2" />
+            <a href={tool.url}>{tool.url}</a>
+          </div>
+          <div tw="flex">
+            <FaTags tw="mt-2 mr-2" />
+            <ul tw="flex flex-wrap list-none max-w-sm">
+              {tool.tags.map(tag => (
+                <li tw="mt-1 mr-1 mb-1" key={tool.id}>
+                  <span tw="bg-color1 px-1 py-1">{tag}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </article>
     </Layout>
