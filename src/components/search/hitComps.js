64b7f9c602106fb233521254ad7fbbcafda7fb7e
Matthias Endler <matthias-endler@gmx.net>
2020-05-26 23:58:45 +0200

    Cleanup

--- src/components/search/hitComps.js
+++ src/components/search/hitComps.js
@@ -1,7 +1,6 @@
 import React from "react"
-import { Highlight } from "react-instantsearch-dom"
+import { Highlight, Snippet } from "react-instantsearch-dom"
 import { Link } from "gatsby"
-// import { Tags } from "styled-icons/fa-solid/Tags"
 
 export const ToolsHit = clickHandler => ({ hit }) => (
   <div>
@@ -10,8 +9,8 @@ export const ToolsHit = clickHandler => ({ hit }) => (
         <Highlight attribute="name" hit={hit} tagName="mark" />
       </h4>
       {hit.description}
-      {/* FIXME */}
-      {/* <Snippet attribute="description" hit={hit} tagName="mark" /> */}
+      <Snippet attribute="description" hit={hit} tagName="mark" />
+
       {/* {hit.tags.map((tag, index) => (
         <Fragment key={tag}>
           {index > 0 && `, `}
@@ -21,28 +20,3 @@ export const ToolsHit = clickHandler => ({ hit }) => (
     </Link>
   </div>
 )
-
-// export const PostHit = clickHandler => ({ hit }) => (
-//   <div>
-//     <Link to={`/blog` + hit.slug} onClick={clickHandler}>
-//       <h4>
-//         <Highlight attribute="title" hit={hit} tagName="mark" />
-//       </h4>
-//     </Link>
-//     <div>
-//       <Calendar size="1em" />
-//       &nbsp;
-//       <Highlight attribute="date" hit={hit} tagName="mark" />
-//       &emsp;
-//       <Tags size="1em" />
-//       &nbsp;
-//       {hit.tags.map((tag, index) => (
-//         <Fragment key={tag}>
-//           {index > 0 && `, `}
-//           {tag}
-//         </Fragment>
-//       ))}
-//     </div>
-//     <Snippet attribute="excerpt" hit={hit} tagName="mark" />
-//   </div>
-// )
