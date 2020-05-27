b600883b82fb08a858e79d7813b7cfff06c58058
Jakub Sacha <jakub.sacha@trivago.com>
2020-05-27 08:17:27 +0200

    Improve visuals of tools list

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import { Helmet } from "react-helmet"
+import { FaCaretUp, FaCaretDown } from "react-icons/fa"
 import "twin.macro"
 
 const getIntroText = tools => {
@@ -30,23 +31,30 @@ export default function Tag(d) {
           <h1 tw="text-3xl font-semibold pb-10">
             {tag.name} static analysis tools
           </h1>
-          <ul>
-            {tools.map(tool => (
-              <li tw="pr-3 pb-6" key={tool.id}>
-                <p>
-                  Votes: {tool.children[0].sum}{" "}
-                  <a href={`/upVote/${tool.children[0].key}`}>upvote</a>
-                  &nbsp;
-                  <a href={`/downVote/${tool.children[0].key}`}>downvote</a>
-                </p>
 
-                <Link to={tool.fields.slug} tw="font-bold">
-                  {tool.name}
+          {tools.map(tool => (
+            <div tw="my-3 flex  border-b border-gray-200 pb-6" key={tool.id}>
+              <p tw="flex-none w-12 text-center text-gray-600">
+                <a tw="block" href={`/upVote/${tool.children[0].key}`}>
+                  <FaCaretUp tw="m-auto text-3xl text-gray-400" />
+                </a>
+
+                <span tw="block text-color4 font-bold">
+                  {tool.children[0].sum}
+                </span>
+
+                <a tw="block" href={`/downVote/${tool.children[0].key}`}>
+                  <FaCaretDown tw="m-auto text-3xl text-gray-400" />
+                </a>
+              </p>
+              <div tw="flex-auto pl-5">
+                <Link to={tool.fields.slug} tw="pb-4">
+                  <h4 tw="font-bold text-xl mb-3">{tool.name}</h4>
                 </Link>
-                <p tw="">{tool.description}</p>
-              </li>
-            ))}
-          </ul>
+                <p tw="text-gray-600">{tool.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </article>
     </Layout>
