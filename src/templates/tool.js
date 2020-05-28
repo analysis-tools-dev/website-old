3e4c9d3dc0cdc18c2cd82f8afa53d19b7330ce22
Matthias Endler <matthias-endler@gmx.net>
2020-05-28 08:55:39 +0200

    Add maintained/proprietary icons

--- src/templates/tool.js
+++ src/templates/tool.js
@@ -3,7 +3,12 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import { Helmet } from "react-helmet"
 import "twin.macro"
-import { FaTags, FaHome } from "react-icons/fa"
+import {
+  FaTags,
+  FaHome,
+  FaCopyright,
+  FaExclamationCircle,
+} from "react-icons/fa"
 
 export default function BlogPost(d) {
   const tool = d.data.toolsYaml
@@ -19,7 +24,17 @@ export default function BlogPost(d) {
           <p tw="pb-3">{tool.description}</p>
           <div tw="flex mt-3">
             <FaHome tw="mt-1 mr-2" />
-            <a tw="underline" href={tool.url}>{tool.url}</a>
+            <a tw="underline" href={tool.url}>
+              {tool.url}
+            </a>
+          </div>
+          <div tw="flex mt-3">
+            <FaCopyright tw="mt-1 mr-2" />
+            {(tool.proprietary && "Proprietary") || "Open Source"}
+          </div>
+          <div tw="flex mt-3">
+            <FaExclamationCircle tw="mt-1 mr-2" />
+            {(tool.deprecated && "Deprecated/Unmaintained") || "Maintained"}
           </div>
           <div tw="flex mt-3">
             <FaTags tw="mt-2 mr-2" />
@@ -44,6 +59,8 @@ export const query = graphql`
     toolsYaml(fields: { slug: { eq: $slug } }) {
       name
       description
+      proprietary
+      deprecated
       url
       tags
       fields {
