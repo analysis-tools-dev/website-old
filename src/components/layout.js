c77e1cd1fbd96674581260f34e4804b21f1f4db1
Matthias Endler <matthias-endler@gmx.net>
2020-05-28 13:15:29 +0200

    Topics link is redundant as we have "Home" already

--- src/components/layout.js
+++ src/components/layout.js
@@ -51,14 +51,6 @@ export default ({ children }) => {
         tw="w-full py-4 border-t border-b bg-gray-100"
         x-data="{ open: false }"
       >
-        <div tw="block sm:hidden">
-          <Link
-            to="/"
-            tw="block md:hidden text-base font-bold uppercase text-center flex justify-center items-center"
-          >
-            Topics <i tw="ml-2"></i>
-          </Link>
-        </div>
         <div tw="w-full flex-grow sm:flex sm:items-center sm:w-auto">
           <div tw="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
             <Link to="/" tw="hover:bg-gray-400 rounded py-2 px-4 mx-2">
