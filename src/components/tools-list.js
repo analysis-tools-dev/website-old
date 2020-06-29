02fb6857f1a442cfacfdfce282bc0a7c54c03418
Matthias Endler <matthias-endler@gmx.net>
2020-06-29 18:23:45 +0200

    Add tags

--- src/components/tools-list.js
+++ src/components/tools-list.js
@@ -14,7 +14,18 @@ export default function Tool({ tool }) {
         <Link to={tool.fields.slug} tw="pb-4">
           <h4 tw="font-bold text-xl mb-3">{tool.name}</h4>
         </Link>
-        <p tw="text-gray-600">{tool.description}</p>
+        <p tw="text-gray-600 mb-3">{tool.description}</p>
+        <div>
+          <ul tw="list-none max-w-sm inline-block align-top">
+            {tool.tags.map(tag => (
+              <li tw="mb-2 mr-1 inline-block" key={tag}>
+                <a href={"/tag/" + tag}>
+                  <span tw="bg-color-gray-200 px-2 py-1 rounded">{tag}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   )
