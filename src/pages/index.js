f6fbc90d866c58c471b39de1f9e495c34ab2ce9e
Matthias Endler <matthias-endler@gmx.net>
2020-07-21 21:37:16 +0200

    Formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,9 @@ const ComponentName = ({ data }) => {
       </Helmet>
       <article tw="flex flex-col shadow my-4 w-full">
         <div tw="bg-white flex flex-col justify-start p-6 w-full">
-          <p tw="text-xl font-semibold pb-5">Most Popular Analysis Tools by Programming Language</p>
+          <p tw="text-xl font-semibold pb-5">
+            Most Popular Analysis Tools by Programming Language
+          </p>
           <div tw="grid grid-cols-2 gap-3 ">
             {["python", "ruby", "php", "c", "javascript", "go"].map(l => (
               <div key={l} tw="border-b border-gray-200 pb-5 px-2">
@@ -38,7 +40,7 @@ const ComponentName = ({ data }) => {
                 ))}
                 <Link
                   to={`/tag/${l}`}
-                  tw="rounded-full  px-4 mr-2 bg-color2 text-white p-2 rounded-lg leading-none transition-all duration-300 hover:bg-blue-500"
+                  tw="rounded-full px-4 mr-2 bg-color2 text-white p-2 rounded-lg leading-none transition-all duration-300 hover:bg-blue-500"
                 >
                   Check more
                 </Link>
