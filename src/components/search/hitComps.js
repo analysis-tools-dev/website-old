5750bf62484e3726ef21a7b520a822cfc8d85592
Matthias Endler <matthias-endler@gmx.net>
2020-09-11 15:08:44 +0200

    Slightly nicer text formatting

--- src/components/search/hitComps.js
+++ src/components/search/hitComps.js
@@ -4,20 +4,14 @@ import { Link } from "gatsby"
 import "twin.macro"
 
 export const ToolsHit = clickHandler => ({ hit }) => (
-  <div tw="p-2 border-b">
+  <div tw="px-4 py-4 border-b">
     <Link to={hit.fields.slug} onClick={clickHandler}>
-      <h4>
+      <h4 tw="pb-1">
         <Highlight attribute="name" hit={hit} tagName="mark" />
       </h4>
-      {hit.description}
-      <Snippet attribute="description" hit={hit} tagName="mark" />
-
-      {/* {hit.tags.map((tag, index) => (
-        <Fragment key={tag}>
-          {index > 0 && `, `}
-          {tag}
-        </Fragment>
-      ))} */}
+      <p tw="text-gray-600">{hit.description}</p>
+      {/* Why does this not show anything? */}
+      {/* <Snippet attribute="description" hit={hit} tagName="mark" /> */}
     </Link>
   </div>
 )
