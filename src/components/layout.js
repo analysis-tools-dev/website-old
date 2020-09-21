68d10eb51db78d73acd4cfee8dd668fe04314890
Matthias Endler <matthias-endler@gmx.net>
2020-09-22 00:47:01 +0200

    Add copyright info

--- src/components/layout.js
+++ src/components/layout.js
@@ -156,20 +156,29 @@ export default ({ children }) => {
 
       <footer tw="w-full border-t bg-white pb-12">
         <div tw="w-full container mx-auto flex flex-col items-center">
-          <div>
-            Beautiful icons provided by{" "}
-            <a tw="underline" href="https://icons8.com/icon/pack/files/dusk">
-              icons8
-            </a>
-            &nbsp;&middot;&nbsp;
-            <a
-              tw="underline"
-              href="https://www.freepik.com/free-photos-vectors/background"
-            >
-              Hero vector illustration created by pch.vector - www.freepik.com
-            </a>
-          </div>
-          <div tw="pb-6">
+          <ul tw="text-center text-gray-600">
+            <li>
+              Beautiful icons provided by{" "}
+              <a tw="underline" href="https://icons8.com/icon/pack/files/dusk">
+                icons8
+              </a>
+            </li>
+            <li>
+              <a
+                tw="underline"
+                href="https://www.freepik.com/free-photos-vectors/background"
+              >
+                Hero vector illustration created by pch.vector - www.freepik.com
+              </a>
+            </li>
+            <li>
+              <a tw="underline" href="https://thenounproject.com">
+                Cloud by Aya Sofya, plugin by bezier master, console by Richard
+                Schumann from the Noun Project
+              </a>
+            </li>
+          </ul>
+          <div tw="pb-6 text-gray-600">
             &copy;{" "}
             <a tw="underline" href="https://analysis-tools.dev/">
               analysis-tools.dev
