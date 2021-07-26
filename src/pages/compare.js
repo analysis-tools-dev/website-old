7ce2d7cf47355fe93f85c82764f1761b8fae9250
MarlineKhavele <marlinekhavele6@gmail.com>
2021-07-27 00:33:37 +0300

    Adding Material UI

--- src/pages/compare.js
+++ src/pages/compare.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout_wide"
+import Container from '@material-ui/core/Container';
+import Table from '@material-ui/core/Table';
 import Vote from "../components/vote"
-import "twin.macro"
-import MaterialTable from 'material-table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 
 
 const Compare = d => {
-let tools = d.data.allToolsYaml.nodes;
-  // tools = tools.filter(t =>t.children[0].sum >= 10)
-  // tools = tools.sort();
-
+  let tools = d.data.allToolsYaml.nodes;
   const [toolsFiltered, setToolsFiltered] = useState(tools)
   const [showProprietary, setShowProprietary] = useState(false)
   const filterTools = showProprietary => {
@@ -33,19 +37,85 @@ let tools = d.data.allToolsYaml.nodes;
     return 0;
   });
 
-
-  const [filter, setFilter] = useState('');
-  const handleChange = event => {
-        setFilter()
-        if(event.target.value){
-          setToolsFiltered(tools.filter(tool => tool.toLowerCase().includes(filter.toLocaleLowerCase()
-          )))
-        }else {
-          setToolsFiltered(tools)}
-  };
   return (
     <Layout>
-      <article tw="shadow w-full p-2 md:p-8">
+      <Container fixed>
+        <h1>Compare {tools.length} Analysis Tools</h1>
+
+        <div >
+          <input
+            onChange={() => filterTools(showProprietary)}
+            type="checkbox"
+            id="showproprietary"
+            name="showproprietary"
+            value="true"
+          ></input>
+          <label htmlFor="showproprietary">
+            Hide proprietary tools
+          </label>
+        </div>
+
+        <TableContainer component={Paper}>
+        <Table  size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              {heading.map((heading) => (
+                <TableCell  align ="center" key={heading}>{heading}</TableCell>
+
+              ))}
+            </TableRow>
+            
+            {/* filter */}
+          </TableHead>
+
+
+          <TableBody>
+            {toolsFiltered.map(tool => (
+              <TableRow>
+                <TableCell align="center">{tool.categories.join(", ")}</TableCell>
+                <TableCell align="center">{tool.license}</TableCell>
+                <TableCell align="center">
+                  <ul>
+                    {tool.tags &&
+                      tool.tags.slice(0, 3).map(tag => (
+                        <li
+                          key={`${tool.fields.slug}${tag}`}
+                        >
+                          <a href={"/tag/" + tag}>
+                            <span >
+                              {tag}
+                            </span>
+                          </a>
+                        </li>
+                      ))}
+                    {tool.tags.length > 3 && (
+                      <a href={tool.fields.slug}>
+                        <span>
+                          more...
+                        </span>
+                      </a>
+                    )}
+                  </ul>
+                </TableCell>
+
+                <TableCell align="center">{tool.types.join(", ")}</TableCell>
+
+                <TableCell align="center">
+                  <Link to={tool.fields.slug}>
+                    {tool.name}
+                  </Link>
+                </TableCell>
+                <TableCell align="center">
+                  <Vote k={tool.children[0].key} sum={tool.children[0].sum} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
+
+      {/* <article tw="shadow w-full p-2 md:p-8">
         <h1 tw="text-3xl font-semibold pb-10">
           Compare {tools.length} Analysis Tools
         </h1>
@@ -61,75 +131,9 @@ let tools = d.data.allToolsYaml.nodes;
             Hide proprietary tools
           </label>
         </div>
-        <table tw="w-full overflow-x-auto block border">
-          <thead>
-            <tr tw="flex-auto">
-              {heading.map((heading) => (
-                <th key={heading} tw="sticky top-0 md:py-2 text-gray-900 bg-gray-100">{heading}</th>
-                
-              ))}
-            </tr>
-          
-          { <div tw="flex-auto">
-          <input
-          type="text"
-           placeholder="filter"
-           value={filter}
-           onChange={handleChange}
-           />
-         
-        </div> }
-          </thead>
-        
-           
-          <tbody tw="divide-y">
-            {toolsFiltered.map(tool => (
-              <tr>
-                <td tw="text-center md:py-2">{tool.categories.join(", ")}</td>
-                <td tw="text-center md:py-2">{tool.license}</td>
-                <td tw="text-center md:py-2">
-                  <ul tw="list-none max-w-sm inline-block align-top">
-                    {tool.tags &&
-                      tool.tags.slice(0, 3).map(tag => (
-                        <li
-                          tw="mb-2 mr-1 inline-block"
-                          key={`${tool.fields.slug}${tag}`}
-                        >
-                          <a href={"/tag/" + tag}>
-                            <span tw="bg-gray-300 hover:bg-gray-400 px-2 py-1 rounded">
-                              {tag}
-                            </span>
-                          </a>
-                        </li>
-                      ))}
-                    {tool.tags.length > 3 && (
-                      <a href={tool.fields.slug}>
-                        <span tw="bg-gray-300 hover:bg-gray-400 px-2 py-1 rounded">
-                          more...
-                        </span>
-                      </a>
-                    )}
-                  </ul>
-                </td>
-              
-                <td tw="text-center md:py-2">{tool.types.join(", ")}</td>
-                 
-                <td tw="text-center md:py-2">
-                  <Link to={tool.fields.slug} tw="underline">
-                    {tool.name}
-                  </Link>
-                </td>
-         
-             
 
-                <td tw="text-center px-6 py-2">
-                  <Vote k={tool.children[0].key} sum={tool.children[0].sum} />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </article>
+       
+      </article> */}
     </Layout>
   )
 }
